e318d55e3aeb785b25e4b683621e5fc43aead061
Lane Smith <ldsmith@uw.edu>
2022-11-29 18:09:58 -0800

    feat: return line flow results with other power flow results

--- src/tools/powermodels_wrappers.jl
+++ src/tools/powermodels_wrappers.jl
@@ -5,5 +5,15 @@ end
 
 function compute_ac_pf(network_data)
     # Solve the AC power-flow model using PowerModels
-    return PowerModels.compute_ac_pf(network_data)
+    result = PowerModels.compute_ac_pf(network_data)
+
+    # Take a copy of the network data for use in calculating the line flows
+    _network_data = deepcopy(network_data)
+    PowerModels.update_data!(_network_data, result["solution"])
+
+    # Calculate the line flows
+    result["solution"]["line_flows"] = PowerModels.calc_branch_flow_ac(_network_data)
+
+    # Return the results Dictionary
+    return result
 end
